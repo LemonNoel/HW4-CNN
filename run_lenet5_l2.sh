32b512afc9bde9d6000d4ac587e3c8e933ea7ea2
Noel <25607475+LemonNoel@users.noreply.github.com>
2024-11-05 13:54:07 -0800

    [hw4] update training script

--- run_lenet5_l2.sh
+++ run_lenet5_l2.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0 python train.py \
     --model LeNet-5 \
-    --data_path "/project/jonmay_1426/huijuanw/data/CINIC-10" \
-    --save_path "/project/jonmay_1426/huijuanw/ckpt/lenet-5-l2-max/" \
+    --data_path "./data/CINIC-10" \
+    --save_path "./ckpt/lenet-5-l2/" \
     --weight_decay 5e-4
